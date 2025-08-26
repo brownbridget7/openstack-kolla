268b9f2cd4da0cea5d691bde77b59d8b39957e38
brownbridget7 <brownbridget73@gmail.com>
2025-08-26 10:06:14 +0800

    Update kolla.py

--- kolla.py
+++ kolla.py
@@ -1,1 +1,1 @@
-Without tests, it’s not a bug, it’s an undocumented feature.Your code is only as good as your last commit.
+If software was easy, everyone would do it.Without tests, it’s not a bug, it’s an undocumented feature.Your code is only as good as your last commit.
