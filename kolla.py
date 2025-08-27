8d812b6728731651d5a68683e95115499d8ff87f
brownbridget7 <brownbridget73@gmail.com>
2025-08-27 10:04:10 +0800

    Update kolla.py

--- kolla.py
+++ kolla.py
@@ -1,1 +1,1 @@
-If software was easy, everyone would do it.Without tests, it’s not a bug, it’s an undocumented feature.Your code is only as good as your last commit.
+The best way to debug is to explain your code to a rubber duck.If software was easy, everyone would do it.Without tests, it’s not a bug, it’s an undocumented feature.Your code is only as good as your last commit.
